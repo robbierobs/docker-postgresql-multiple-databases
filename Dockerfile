a24d49c4cf08963ad60450b2b0b32f748505117d
Sean Robinson <irobbierobinson@gmail.com>
2022-06-29 14:41:40 -0400

    Using a bitnami container instead of the standard postgres image

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
-FROM postgres:9.6
+# FROM postgres:12
+FROM bitnami/postgresql
 COPY create-multiple-postgresql-databases.sh /docker-entrypoint-initdb.d/
--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -6,10 +6,9 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" <<-EOSQL
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRESQL_USERNAME;
 EOSQL
 }
 
